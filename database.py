311d37b49eeadb84b8d1e29b978f8ae6c744ff30
Ismail Tirmizi <ismailhero@gmail.com>
2021-01-06 00:44:48 +0500

    New db creds added

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://db_dev:lolpass@localhost/scrapper_db"
-SQLALCHEMY_DATABASE_URL = "postgres://fmihrwoqyljazr:6d3971e99cf3a27c3762cea7c0bd918c7f65a0210ad9da6e6d603f58d3d57f04@ec2-3-210-255-177.compute-1.amazonaws.com:5432/d13hhdsa2jq3un"
+SQLALCHEMY_DATABASE_URL = "postgres://wotlzujyqzemtl:a9b17b0c827e24c433235ca89a3c73ed88c64fac119e8621c2c262cf0c5f9833@ec2-52-44-46-66.compute-1.amazonaws.com:5432/dfqd9trqnt0upm"
 
 engine = create_engine( SQLALCHEMY_DATABASE_URL )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

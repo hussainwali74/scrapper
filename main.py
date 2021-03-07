341a90a888aed1bc5c97ae583a060b8bfbd36a76
Ismail Tirmizi <ismailhero@gmail.com>
2021-03-07 16:39:13 +0500

    Cross-Origin Resource Sharing CORS added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import SessionLocal, engine, Base, get_db
 from models.db_models.car import CarDB
@@ -13,6 +14,21 @@ from api import api
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.api_router)
 

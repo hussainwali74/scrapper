9f1f29e070b4638a88679bed9e64525f6c78083d
Ismail Tirmizi <ismailhero@gmail.com>
2021-05-12 18:14:51 +0500

    Bugfix in gunicorn logging

--- run_dev_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# For development (Firefox browser opens)
-#uvicorn main:app --host 0.0.0.0 --reload
-
-# For production (Firefox runs in headless mode)
-GECKODRIVER_PATH="/home/teemo/softwares/geckodriver" \
-ENV="prod" gunicorn main:app --preload --bind 0.0.0.0:8000 -w 1 -k uvicorn.workers.UvicornWorker --timeout 21_600
-#gunicorn main:app --bind 0.0.0.0:8000 -w 1

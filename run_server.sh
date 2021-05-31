c6d8a5ae5e371921036b59dcf9e67bea8bdb4473
Ismail Tirmizi <ismailhero@gmail.com>
2021-05-31 12:31:01 +0500

    run_server.sh update to certfile path

--- run_server.sh
+++ run_server.sh
@@ -6,6 +6,6 @@
 # Uses Gunicorn
 GECKODRIVER_PATH="/root/geckodriver" ENV='prod' \
 gunicorn main:app --bind 0.0.0.0:443 -w 3 -k uvicorn.workers.UvicornWorker \
---certfile=/etc/letsencrypt/live/roomie.pk/fullchain.pem \
---keyfile=/etc/letsencrypt/live/roomie.pk/privkey.pem \
+--certfile=/etc/letsencrypt/live/www.roomie.pk/fullchain.pem \
+--keyfile=/etc/letsencrypt/live/www.roomie.pk/privkey.pem \
 --timeout 43_200 --daemon --access-logfile='app.log' --log-file='app.log'
